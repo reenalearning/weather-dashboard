a8d205050ce4d04bcb06975bc15177d71380b31c
Reena patel <reenavk0342@gmail.com>
2026-04-01 18:15:51 -0600

    fix bug

--- Backend/obj/Release/net8.0/WeatherApi.AssemblyInfo.cs
+++ Backend/obj/Release/net8.0/WeatherApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeab0ba41586b3dec2ccd844b2617c5d9e264473")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApi")]
